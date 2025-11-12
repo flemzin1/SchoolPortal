b831a924da8f8cd27864eb612803861441f3db28
Chizoba Emmanuel <flemzin1@gmail.com>
2025-11-12 05:52:31 +0000

    Wrap it in suspense

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,12 @@
 
 import * as React from 'react';
+import { Suspense } from 'react';
 import { DashboardLayout } from '@/components/dashboard-layout';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
+  return (
+    <Suspense>
+      <DashboardLayout>{children}</DashboardLayout>
+    </Suspense>
+  );
 }
--- src/app/parstud/layout.tsx
+++ src/app/parstud/layout.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 import * as React from 'react';
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { DashboardLayout } from '@/components/dashboard-layout';
 
@@ -8,5 +9,9 @@ export default function ParStudLayout({ children }: { children: React.ReactNode 
   const searchParams = useSearchParams();
   const isGuest = searchParams.get('guest') === 'true';
 
-  return <DashboardLayout isGuest={isGuest}>{children}</DashboardLayout>;
+  return (
+    <Suspense>
+      <DashboardLayout isGuest={isGuest}>{children}</DashboardLayout>
+    </Suspense>
+  );
 }
